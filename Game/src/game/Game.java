1b1e3881b74526cae5fbfd917ae8a63fb74662fa
Keyvhinng <keyvhinng@me.com>
2013-09-11 22:39:29 -0500

    add Welcome message

--- Game/src/game/Game.java
+++ Game/src/game/Game.java
@@ -2,15 +2,24 @@ package game;
 /**
  * @author kref
  *         Kevin Baba
- *         Rodrigo
- *         Daniel
+ *         Rodrigo Maldonado
+ *         Daniel Carbajal
  */
 import java.io.*;
 import java.util.Scanner;
 import java.util.Arrays;
 import java.util.StringTokenizer;
+import game.test.testClass;
+
+//public class game{
+//
+//}
 
 public class Game {
+    
+//    class game(){
+//    
+//    }
 
     public static void main(String[] args) throws IOException {
         //reader
@@ -18,8 +27,16 @@ public class Game {
         //printer
         OutputStream outputStream = System.out;
         PrintWriter out = new PrintWriter(outputStream);
+        //variables
+        int option;
         //begin
         
+        out.println("Bienvenido (:");
+        out.println("Si desea iniciar el juego presione 1, si desea terminar presione 0");
+        option = in.nextInt();
+        if(option==2){
+            
+        }
         //close output
         out.close();
     }
