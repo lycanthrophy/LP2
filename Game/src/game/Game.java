59a0b1d1f5f1ec973f3909510db7cb9e92f88ae3
Keyvhinng <keyvhinng@me.com>
2013-09-11 22:15:22 -0500

    Add scanner and printer

--- Game/src/game/Game.java
+++ Game/src/game/Game.java
@@ -1,12 +1,28 @@
 package game;
-
 /**
  * @author kref
+ *         Kevin Baba
+ *         Rodrigo
+ *         Daniel
  */
+import java.io.*;
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
 public class Game {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        //reader
+        Scanner in = new Scanner(System.in);
+        //printer
+        OutputStream outputStream = System.out;
+        PrintWriter out = new PrintWriter(outputStream);
+        //begin
         
+        //close output
+        out.close();
     }
 
 }
+
